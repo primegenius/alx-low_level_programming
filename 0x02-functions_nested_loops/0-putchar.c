050a30f1ac765c9c14b748679b7e663998552c34
primegenius <theophilusmarfo52@gmail.c~~~~omr>
2023-02-21 13:29:53 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0
  */
+
 int main(void)
 {
 	_putchar('_');
